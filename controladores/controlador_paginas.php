b4a2df464a04617fd7ce3ec87d2307b31f1a5f15
JorgeDevmm <jorge29luis10@gmail.com>
2023-09-11 09:43:28 -0500

    Agregando ruteador y carga de vistas

--- controladores/controlador_paginas.php
+++ controladores/controlador_paginas.php
@@ -4,10 +4,13 @@
 class ControladorPaginas{
 
   public function inicio(){
+
+    require_once '../vistas/paginas/inicio.php';
     
   }
   public function error(){
 
+    require_once '../vistas/paginas/error.php';
   }
 }
 
---
+++ ruteador.php
@@ -0,0 +1,14 @@
+<?php
+
+
+require_once('controladores/controlador_paginas.php');
+
+// crear instancia de controlador
+$controlador = new ControladorPaginas();
+
+$controlador->inicio();
+$controlador->error();
+
+
+
+?>
