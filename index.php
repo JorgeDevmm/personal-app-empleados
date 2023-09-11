fc8b73ee1c1da3db1acdec21b6a020645684c8c6
JorgeDevmm <jorge29luis10@gmail.com>
2023-09-11 13:21:34 -0500

    Recpecionando valores de solicitud

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+
+if (isset($_GET['controlador']) && isset($_GET['accion'])){
+
+  $controlador = $_GET['controlador'];
+  $accion = $_GET['accion'];
+
+  print_r($controlador);
+  print_r($accion);
+}
+
+
 require_once './vistas/template.php';
 
 
