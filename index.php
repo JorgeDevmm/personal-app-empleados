95e45b4d7ad18a895784104ca77b5b078fb52ec1
JorgeDevmm <jorge29luis10@gmail.com>
2023-09-11 14:57:33 -0500

    Validando controlador y acción

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
+// valores por default
+$controlador = "paginas";
+$accion = "inicio";
+
+
+
 
 if (isset($_GET['controlador']) && isset($_GET['accion'])){
 
-  $controlador = $_GET['controlador'];
-  $accion = $_GET['accion'];
+  // validación
 
+  if(($_GET['controlador'] != "") && ($_GET['accion']) != ""){
+    $controlador = $_GET['controlador'];
+    $accion = $_GET['accion'];
+  }
   // validación
   // print_r($controlador);
   // print_r($accion);
