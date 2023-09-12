64c4f4fde231e685d2726ff470fac7646faf1282
JorgeDevmm <jorge29luis10@gmail.com>
2023-09-11 23:21:51 -0500

    Creando Vista Listar y Vista Crear

--- vistas/empleados/crear.php
+++ vistas/empleados/crear.php
@@ -1,1 +1,21 @@
-Formulario de crear
+<div class="card">
+  <div class="card-header bg-dark text-white">
+    Agregar Empleado
+  </div>
+  <div class="card-body">
+    <form action="" method="post">
+      <div class="mb-3">
+        <label for="nombre" class="form-label">Nombre</label>
+        <input type="text"
+          class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="Nombre">
+      </div>
+
+      <div class="mb-3">
+        <label for="correo" class="form-label">Email</label>
+        <input type="email" class="form-control" name="correo" id="correo" aria-describedby="emailHelpId" placeholder="correo del empleado">
+      </div>
+
+      <input name="" id="" class="btn btn-success" type="submit" value="Agregar Empleado">
+    </form>
+  </div>
+</div>
--- vistas/empleados/inicio.php
+++ vistas/empleados/inicio.php
@@ -1,2 +1,20 @@
-
-tabla de inicio
+<div class="table-responsive">
+  <table class="table table-secondary">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Correo</th>
+        <th scope="col">Accion</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="">
+        <td scope="row">1</td>
+        <td>Jorge Monzón</td>
+        <td>jorge29luis10@gmail.com</td>
+        <td>Editar | Borrar</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
